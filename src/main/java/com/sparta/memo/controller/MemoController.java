f4784d78d04c2e73920abb866785e1cb98ee5a43
yokxim <yooksi53@gmail.com>
2024-09-26 16:47:03 +0900

    Create new memo Get memos

---
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -0,0 +1,40 @@
+package com.sparta.memo.controller;
+
+import com.sparta.memo.dto.MemoRequestDto;
+import com.sparta.memo.dto.MemoResponseDto;
+import com.sparta.memo.entity.Memo;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class MemoController {
+
+    private final Map<Long, Memo> memoList = new HashMap<>();
+
+    @PostMapping("/memos")
+    public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto) {
+        // RequestDto -> Entity
+        Memo memo = new Memo(requestDto);
+
+        // Memo Max ID Check
+        Long maxId = !memoList.isEmpty() ? Collections.max(memoList.keySet()) + 1 : 1;
+        memo.setId(maxId);
+
+        // DB 저장
+        memoList.put(memo.getId(), memo);
+
+        // Entity -> ResponseDto
+        return new MemoResponseDto(memo);
+    }
+
+    @GetMapping("/memos")
+    public List<MemoResponseDto> getMemos() {
+        // Map to List
+        return memoList.values().stream().map(MemoResponseDto::new).toList();
+    }
+}
